a2512503db63fe1cde06a5ba0cf86e6569b1bb21
Arnaud <arnaud@freshplanet.com>
2013-06-26 13:05:00 -0400

    Fix extension.xml and made the ios code look nicer.

--- ios/ANEKeyboardSize/KeyboardSize.m
+++ ios/ANEKeyboardSize/KeyboardSize.m
@@ -1,38 +1,44 @@
+//////////////////////////////////////////////////////////////////////////////////////
 //
-//  ANEKeyboardSize.m
-//  ANEKeyboardSize
+//  Copyright 2012 Freshplanet (http://freshplanet.com | opensource@freshplanet.com)
 //
-//  Created by Li on 6/15/13.
-//  Copyright (c) 2013 Li. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-#import "FlashRuntimeExtensions.h"
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//////////////////////////////////////////////////////////////////////////////////////
 
-FREContext eventContext;
-
-FREObject initFunction(FREContext ctx, void* functData, uint32_t argc, FREObject argv[])
-{
-    eventContext = ctx;
-    
-    return NULL;
-}
-
-FREObject getKeyboardY(FREContext ctx, void* functData, uint32_t argc, FREObject argv[])
-{
-    return NULL;
-}
-
-FREObject getKeyboardHeight(FREContext ctx, void* functData, uint32_t argc, FREObject argv[])
-{
-    return NULL;
-}
+#import "KeyboardSize.h"
 
 
-FREObject setKeyboardAdjustNothing(FREContext ctx, void* functData, uint32_t argc, FREObject argv[])
+#pragma mark - KeyboardSize
+
+DEFINE_ANE_FUNCTION(getKeyboardY)
 {
     return NULL;
 }
 
-void ExtContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctions, const FRENamedFunction** functionsToSet)
+DEFINE_ANE_FUNCTION(getKeyboardHeight)
+{
+    return NULL;
+}
+
+DEFINE_ANE_FUNCTION(setKeyboardAdjustNothing)
+{
+    return NULL;
+}
+
+#pragma mark - C interface
+
+void KeyboardSizeContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctions, const FRENamedFunction** functionsToSet)
 {
     *numFunctions = 3;
     FRENamedFunction* func = (FRENamedFunction*) malloc(sizeof(FRENamedFunction) * *numFunctions);
@@ -52,8 +58,12 @@ void ExtContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx
     *functionsToSet = func;
 }
 
-void KeyboardExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet)
+void KeyboardSizeContextFinalizer(FREContext ctx) { }
+
+void KeyboardSizeInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet)
 {
     *extDataToSet = NULL;
-    *ctxInitializerToSet = &ExtContextInitializer;
+    *ctxInitializerToSet = &KeyboardSizeContextInitializer;
 }
+
+void KeyboardSizeFinalizer(void *extData) { }
