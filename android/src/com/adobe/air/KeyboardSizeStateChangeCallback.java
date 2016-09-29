9ac58e69e8e416859984d0aa2159d98ca3a896f5
Peter Nicolai <peter@freshplanet.com>
2016-09-29 13:15:57 -0400

    (actually for android) mitigate Air 23 bug with statusbar

---
+++ android/src/com/adobe/air/KeyboardSizeStateChangeCallback.java
@@ -0,0 +1,7 @@
+package com.adobe.air;
+
+/**
+ * Created by peternicolai on 8/25/16.
+ */
+public interface KeyboardSizeStateChangeCallback extends AndroidActivityWrapper.StateChangeCallback {
+}
--- android/src/com/freshplanet/ane/KeyboardSize/ExtensionContext.java
+++ android/src/com/freshplanet/ane/KeyboardSize/ExtensionContext.java
@@ -3,19 +3,18 @@ package com.freshplanet.ane.KeyboardSize;
 import java.util.HashMap;
 import java.util.Map;
 
-import android.graphics.Rect;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import com.adobe.fre.FREContext;
-import com.adobe.fre.FREFunction;
-import com.adobe.fre.FREObject;
-import com.adobe.fre.FREWrongThreadException;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.adobe.air.AndroidActivityWrapper;
+import com.adobe.air.KeyboardSizeStateChangeCallback;
+import com.adobe.fre.*;
 
-public class ExtensionContext extends FREContext implements View.OnLayoutChangeListener {
+public class ExtensionContext extends FREContext implements View.OnLayoutChangeListener, KeyboardSizeStateChangeCallback {
 
+    private AndroidActivityWrapper wrapper;
 
     @Override
     public void dispose() {
@@ -30,66 +29,103 @@ public class ExtensionContext extends FREContext implements View.OnLayoutChangeL
 		functionMap.put("getKeyboardHeight", new getKeyboardHeight() );
 		functionMap.put("setSoftInputMode",new setSoftInputMode() );
 		functionMap.put("removeClearButtonForiOS",new removeClearButtonForiOS() );
+        functionMap.put("getMultilineTextViewHeight", getMultilineTextViewHeight);
+        functionMap.put("resetFullScreen", resetFullScreenFunction);
         functionMap.put("init", initFunction);
 
 		return functionMap;
 	}
 
+    @Override
+    public void onActivityStateChanged(AndroidActivityWrapper.ActivityState activityState) {
+        resetFullScreen();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration configuration) {
+        resetFullScreen();
+    }
+
+    public final FREFunction getMultilineTextViewHeight = new FREFunction() {
+        @Override
+        public FREObject call(FREContext freContext, FREObject[] freObjects) {
+            double found = findTextHeight((ViewGroup)getActivity().getWindow().getDecorView().getRootView());
+            try {
+                return FREObject.newObject(found);
+            } catch (FREWrongThreadException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
+
+    private double findTextHeight(ViewGroup parent)
+    {
+        for(int index=0; index<parent.getChildCount(); ++index) {
+            View nextChild = parent.getChildAt(index);
+            if(nextChild instanceof TextView) {
+                Log.d("KeyboardSize", "Found a textview: " + ((TextView)nextChild).getText());
+                nextChild.measure(0,0);
+                return nextChild.getHeight();
+            } else if (nextChild instanceof ViewGroup){
+                double ret = findTextHeight((ViewGroup)nextChild);
+                if(ret > 0) {
+                    return ret;
+                }
+            }
+        }
+        return -1;
+    }
+
     public final FREFunction initFunction = new FREFunction() {
         @Override
         public FREObject call(FREContext freContext, FREObject[] freObjects) {
-           // freContext.getActivity().getWindow().getCurrentFocus().addOnLayoutChangeListener(ExtensionContext.this);
-          //  freContext.getActivity().getWindow().getDecorView().addOnLayoutChangeListener(ExtensionContext.this);
+
+            boolean fullscreen = false;
+            if(freObjects.length > 0) {
+                try {
+                    fullscreen = freObjects[0].getAsBool();
+                } catch (FRETypeMismatchException e) {
+                    e.printStackTrace();
+                } catch (FREInvalidObjectException e) {
+                    e.printStackTrace();
+                } catch (FREWrongThreadException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fullscreen) {
+                AndroidActivityWrapper.GetAndroidActivityWrapper().addActivityStateChangeListner(ExtensionContext.this);
+                freContext.getActivity().getWindow().getDecorView().addOnLayoutChangeListener(ExtensionContext.this);
+                getActivity().getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
+                        (new View.OnSystemUiVisibilityChangeListener() {
+                            @Override
+                            public void onSystemUiVisibilityChange(int visibility) {
+                                resetFullScreen();
+                            }
+                        });
+            }
+
+            return null;
+        }
+    };
+
+    private void resetFullScreen() {
+        final View decorView = getActivity().getWindow().getDecorView();
+        decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    public final FREFunction resetFullScreenFunction = new FREFunction() {
+        @Override
+        public FREObject call(FREContext freContext, FREObject[] freObjects) {
+            resetFullScreen();
             return null;
         }
     };
 
     @Override
     public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-        try {
-            JSONObject output = new JSONObject();
 
-
-            output.put("left", left);
-            output.put("right", right);
-            output.put("top", top);
-            output.put("bottom", bottom);
-            output.put("oldLeft", left);
-            output.put("oldRight", right);
-            output.put("oldTop", top);
-            output.put("oldBottom", bottom);
-
-            dispatchStatusEventAsync("LAYOUT_CHANGE", output.toString(2));
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        resetFullScreen();
     }
 
-    public static String rectsToJSON (Rect rect1, Rect rect2) {
-        JSONObject output = new JSONObject();
-        JSONObject rect1JSON = new JSONObject();
-        JSONObject rect2JSON = new JSONObject();
-
-        try {
-            rect1JSON.put("left", rect1.left);
-            rect1JSON.put("top", rect1.top);
-            rect1JSON.put("width", rect1.width());
-            rect1JSON.put("height", rect1.height());
-            output.put("rect1", rect1JSON);
-
-            rect2JSON.put("left", rect2.left);
-            rect2JSON.put("top", rect2.top);
-            rect2JSON.put("width", rect2.width());
-            rect2JSON.put("height", rect2.height());
-            output.put("rect2", rect2JSON);
-
-            return output.toString();
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return "{}";
-
-    }
 }
