0b5c5066f0d8cc4ca773734060b6fde547a600c1
Peter Nicolai <peter@freshplanet.com>
2016-10-21 14:46:48 -0400

    remove unnecessary stuff

--- android/src/com/freshplanet/ane/KeyboardSize/getKeyboardY.java
+++ android/src/com/freshplanet/ane/KeyboardSize/getKeyboardY.java
@@ -26,11 +26,10 @@ public class getKeyboardY implements FREFunction{
 		View rootview = arg0.getActivity().getWindow().getDecorView();
 		rootview.getWindowVisibleDisplayFrame(r);
 
-		int YNumber = r.bottom - getNavigationBarSize(arg0.getActivity().getApplicationContext()).y;
 		FREObject Y = null;
 
 		try {
-			Y = FREObject.newObject(YNumber);
+			Y = FREObject.newObject(r.bottom);
 		} catch (IllegalStateException e) {
 			Log.d(tag, e.getMessage());
 			e.printStackTrace();
@@ -38,52 +37,7 @@ public class getKeyboardY implements FREFunction{
 			e.printStackTrace();
 		}
 
-		Log.d(tag, YNumber+" ");
-
 		return Y;
-	}
-
-	public static Point getNavigationBarSize(Context context) {
-		Point appUsableSize = getAppUsableScreenSize(context);
-		Point realScreenSize = getRealScreenSize(context);
-
-		// navigation bar on the right
-		if (appUsableSize.x < realScreenSize.x) {
-			return new Point(realScreenSize.x - appUsableSize.x, appUsableSize.y);
-		}
-
-		// navigation bar at the bottom
-		if (appUsableSize.y < realScreenSize.y) {
-			return new Point(appUsableSize.x, realScreenSize.y - appUsableSize.y);
-		}
-
-		// navigation bar is not present
-		return new Point();
-	}
-
-	public static Point getAppUsableScreenSize(Context context) {
-		WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-		Display display = windowManager.getDefaultDisplay();
-		Point size = new Point();
-		display.getSize(size);
-		return size;
-	}
-
-	public static Point getRealScreenSize(Context context) {
-		WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-		Display display = windowManager.getDefaultDisplay();
-		Point size = new Point();
-
-		if (Build.VERSION.SDK_INT >= 17) {
-			display.getRealSize(size);
-		} else if (Build.VERSION.SDK_INT >= 14) {
-			try {
-				size.x = (Integer) Display.class.getMethod("getRawWidth").invoke(display);
-				size.y = (Integer) Display.class.getMethod("getRawHeight").invoke(display);
-			} catch (IllegalAccessException e) {} catch (InvocationTargetException e) {} catch (NoSuchMethodException e) {}
-		}
-
-		return size;
 	}
 }
 
